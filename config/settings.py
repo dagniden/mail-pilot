fc4d6a81c10d5de3d490b2fb7843cc85797db545
Daniil Gnidenko <dagnidenko@gmail.com>
2026-01-15 21:45:42 +0300

    Добавлена кастомная модель пользователя и миграции

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.CustomUser"
 
 # Email server configuration
 EMAIL_HOST = config("EMAIL_HOST")
