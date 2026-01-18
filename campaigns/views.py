0bb9fd96aa12a80b72263eee66df3b271373c1fd
Daniil Gnidenko <dagnidenko@gmail.com>
2026-01-18 16:55:16 +0300

    Добавлены CRUD для модели MessageTemplate

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from campaigns.forms import ClientForm
-from campaigns.models import Client
+from campaigns.forms import ClientForm, MessageTemplateForm, CampaignForm
+from campaigns.models import Client, MessageTemplate, Campaign
 
 
 class ClientListView(ListView):
@@ -34,3 +34,72 @@ class ClientUpdateView(UpdateView):
 class ClientDeleteView(DeleteView):
     model = Client
     success_url = reverse_lazy('campaigns:client_list')
+
+
+class MessageTemplateListView(ListView):
+    model = MessageTemplate
+    context_object_name = "message_templates"
+
+
+class MessageTemplateDetailView(DetailView):
+    model = MessageTemplate
+    context_object_name = "message_template"
+
+
+class MessageTemplateCreateView(CreateView):
+    model = MessageTemplate
+    form_class = MessageTemplateForm
+    success_url = reverse_lazy('campaigns:message_templates_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class MessageTemplateUpdateView(UpdateView):
+    model = MessageTemplate
+    form_class = MessageTemplateForm
+    success_url = reverse_lazy('campaigns:message_templates_list')
+
+
+class MessageTemplateDeleteView(DeleteView):
+    model = MessageTemplate
+    context_object_name = "message_template"
+    success_url = reverse_lazy('campaigns:message_templates_list')
+
+
+# Campaign CRUD Views
+class CampaignListView(ListView):
+    model = Campaign
+    context_object_name = "campaigns"
+
+
+class CampaignDetailView(DetailView):
+    model = Campaign
+    context_object_name = "campaign"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.update_status()
+        return obj
+
+
+class CampaignCreateView(CreateView):
+    model = Campaign
+    form_class = CampaignForm
+    success_url = reverse_lazy('campaigns:campaign_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class CampaignUpdateView(UpdateView):
+    model = Campaign
+    form_class = CampaignForm
+    success_url = reverse_lazy('campaigns:campaign_list')
+
+
+class CampaignDeleteView(DeleteView):
+    model = Campaign
+    success_url = reverse_lazy('campaigns:campaign_list')
