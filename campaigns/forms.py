5e9d7de9c5e8fbb1db48ed0422fc45e9e560be44
Daniil Gnidenko <dagnidenko@gmail.com>
2026-01-18 16:54:33 +0300

    Добавлены CRUD для модели Campaign

--- campaigns/forms.py
+++ campaigns/forms.py
@@ -1,10 +1,28 @@
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+from django import forms
 
-from campaigns.models import Client
+from campaigns.models import Client, MessageTemplate, Campaign
 
 
 class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = ['email', 'full_name', 'comment']
+
+
+class MessageTemplateForm(ModelForm):
+    class Meta:
+        model = MessageTemplate
+        fields = ['subject', 'body']
+
+
+class CampaignForm(ModelForm):
+    class Meta:
+        model = Campaign
+        fields = ['message_template', 'clients', 'start_time', 'end_time']
+        widgets = {
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'clients': forms.CheckboxSelectMultiple(),
+        }
--- campaigns/urls.py
+++ campaigns/urls.py
@@ -10,4 +10,18 @@ urlpatterns = [
     path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
     path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
     path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+
+    # Шаблоны рассылок
+    path('message_templates/', views.MessageTemplateListView.as_view(), name='message_templates_list'),
+    path('message_templates/create/', views.MessageTemplateCreateView.as_view(), name='message_template_create'),
+    path('message_templates/<int:pk>/', views.MessageTemplateDetailView.as_view(), name='message_template_detail'),
+    path('message_templates/<int:pk>/update/', views.MessageTemplateUpdateView.as_view(), name='message_template_update'),
+    path('message_templates/<int:pk>/delete/', views.MessageTemplateDeleteView.as_view(), name='message_template_delete'),
+
+    # Рассылки
+    path('campaigns/', views.CampaignListView.as_view(), name='campaign_list'),
+    path('campaigns/create/', views.CampaignCreateView.as_view(), name='campaign_create'),
+    path('campaigns/<int:pk>/', views.CampaignDetailView.as_view(), name='campaign_detail'),
+    path('campaigns/<int:pk>/update/', views.CampaignUpdateView.as_view(), name='campaign_update'),
+    path('campaigns/<int:pk>/delete/', views.CampaignDeleteView.as_view(), name='campaign_delete'),
 ]
