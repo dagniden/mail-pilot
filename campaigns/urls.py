b15db932c75db7e1ff78939a562312654d6d3ff1
Daniil Gnidenko <dagnidenko@gmail.com>
2026-01-20 21:58:04 +0300

    Подключение новых вью к urls

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,27 +1,34 @@
 from django.urls import path
-from . import views
+from .views import ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView, \
+    MessageTemplateListView, MessageTemplateCreateView, MessageTemplateDetailView, MessageTemplateUpdateView, \
+    MessageTemplateDeleteView, CampaignListView, CampaignCreateView, CampaignDetailView, CampaignUpdateView, \
+    CampaignDeleteView, CampaignAttemptListView, CampaignAttemptDetailView
 
 app_name = "campaigns"
 
 urlpatterns = [
     # Клиенты
-    path('clients/', views.ClientListView.as_view(), name='client_list'),
-    path('clients/create/', views.ClientCreateView.as_view(), name='client_create'),
-    path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
-    path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
-    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('clients/create/', ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
 
     # Шаблоны рассылок
-    path('message_templates/', views.MessageTemplateListView.as_view(), name='message_templates_list'),
-    path('message_templates/create/', views.MessageTemplateCreateView.as_view(), name='message_template_create'),
-    path('message_templates/<int:pk>/', views.MessageTemplateDetailView.as_view(), name='message_template_detail'),
-    path('message_templates/<int:pk>/update/', views.MessageTemplateUpdateView.as_view(), name='message_template_update'),
-    path('message_templates/<int:pk>/delete/', views.MessageTemplateDeleteView.as_view(), name='message_template_delete'),
+    path('message_templates/', MessageTemplateListView.as_view(), name='message_templates_list'),
+    path('message_templates/create/', MessageTemplateCreateView.as_view(), name='message_template_create'),
+    path('message_templates/<int:pk>/', MessageTemplateDetailView.as_view(), name='message_template_detail'),
+    path('message_templates/<int:pk>/update/', MessageTemplateUpdateView.as_view(), name='message_template_update'),
+    path('message_templates/<int:pk>/delete/', MessageTemplateDeleteView.as_view(), name='message_template_delete'),
 
     # Рассылки
-    path('campaigns/', views.CampaignListView.as_view(), name='campaign_list'),
-    path('campaigns/create/', views.CampaignCreateView.as_view(), name='campaign_create'),
-    path('campaigns/<int:pk>/', views.CampaignDetailView.as_view(), name='campaign_detail'),
-    path('campaigns/<int:pk>/update/', views.CampaignUpdateView.as_view(), name='campaign_update'),
-    path('campaigns/<int:pk>/delete/', views.CampaignDeleteView.as_view(), name='campaign_delete'),
+    path('campaigns/', CampaignListView.as_view(), name='campaign_list'),
+    path('campaigns/create/', CampaignCreateView.as_view(), name='campaign_create'),
+    path('campaigns/<int:pk>/', CampaignDetailView.as_view(), name='campaign_detail'),
+    path('campaigns/<int:pk>/update/', CampaignUpdateView.as_view(), name='campaign_update'),
+    path('campaigns/<int:pk>/delete/', CampaignDeleteView.as_view(), name='campaign_delete'),
+
+    # Попытки рассылки
+    path('attempts/', CampaignAttemptListView.as_view(), name='attempt_list'),
+    path('attempts/<int:pk>/', CampaignAttemptDetailView.as_view(), name='attempt_detail')
 ]
