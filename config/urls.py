635a6850f95a12f5fc38ea5e28df934e86da0b86
Daniil Gnidenko <dagnidenko@gmail.com>
2026-01-27 21:45:43 +0300

    Добавлена статистика

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import include, path
+
+from campaigns.views import IndexView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("", IndexView.as_view(), name="home"),
     path("admin/", admin.site.urls),
-    path('campaigns/', include("campaigns.urls", namespace="campaigns")),
-    path('users/', include("users.urls", namespace="users"))
+    path("campaigns/", include("campaigns.urls", namespace="campaigns")),
+    path("users/", include("users.urls", namespace="users")),
 ]
